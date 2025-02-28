bb5d14e82db16a7960126321f1da2edf185575eb
Volodymyr <sholupenkovladimir@gmail.com>
2025-02-28 19:10:53 +0100

    completed homework 2.1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+number = int(input("Введіть 4-х значне число:"))
+
+n1 = number // 1000
+n2 = (number // 100) % 10
+n3 = (number // 10) % 10
+n4 = number % 10
+
+print(n1)
+print(n2)
+print(n3)
+print(n4)
